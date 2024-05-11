99c9c9b3d33c959fe625fd56f9568ac98ce2f4ff
H1epVu <hiepvu.tech02@gmail.com>
2024-05-11 22:49:44 +0700

    add 'Comment Model'

---
+++ models/comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    productId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
