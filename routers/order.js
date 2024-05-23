9380273d8e26f0645e31d6b7cb3688a1b45c74de
H1epVu <hiepvu.tech02@gmail.com>
2024-05-24 06:21:24 +0700

    add totalRevenue router

--- routers/order.js
+++ routers/order.js
@@ -6,11 +6,12 @@ const { authToken } = require('../auth/isAuth');
 
 const router = express.Router();
 
+router.post('/add', authToken, orderController.postOrder)
+router.post('/update', authToken, orderController.updateOrder)
+router.get('/totalRevenue', authToken, orderService.getRevenue)
 router.get('/status', authToken, orderService.getOrderByStatus)
 router.get('/user/:userId', authToken, orderService.getUserOrders)
 router.get('/:orderId', authToken, orderController.getOrderById)
 router.get('/', authToken, orderController.getOrders)
-router.post('/add', authToken, orderController.postOrder)
-router.post('/update', authToken, orderController.updateOrder)
 
 module.exports = router
