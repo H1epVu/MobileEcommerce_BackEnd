3433521d86318056859ef9ad3c2ad64ec837d7f8
H1epVu <hiepvu.tech02@gmail.com>
2024-05-24 06:20:38 +0700

    get Orders by date

--- services/order.js
+++ services/order.js
@@ -23,3 +23,29 @@ exports.getOrderByStatus = async (req, res) => {
         return res.status(404).json({ message: error })
     }
 }
+
+exports.getOrdersByDate = async (req, res) => {
+
+    try {
+        const startOfDay = new Date();
+        startOfDay.setHours(0, 0, 0, 0);
+
+        const endOfDay = new Date();
+        endOfDay.setHours(23, 59, 59, 999);
+
+        const orders = await Order.find({
+            orderDate: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        });
+
+        return res.status(200).json(orders);
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({ message: error })
+    }
+};
+
+
+
