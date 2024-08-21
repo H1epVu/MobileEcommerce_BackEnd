10e1e0c35a4622ba4b926e9f2d71ed34c3080146
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-21 23:08:35 +0700

    change status code

--- services/user.js
+++ services/user.js
@@ -2,13 +2,14 @@ const User = require('../models/user')
 
 //Get User By Email
 exports.getUserByEmail = async (req, res) => {
-    const { email } = req.body;
+    const { email } = req.query
     try {
         const user = await User.findOne({ email: email });
-        if (!user) {
-            return res.status(404).json({ message: 'Không tìm thấy người dùng!' });
+        if (user) {
+            return res.status(200).json({ message: 'Người dùng đã tồn tại!', user });
         }
-        return res.status(200).json(user);
+
+        return res.status(404).json({ message: 'Không tìm thấy người dùng!' });
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: 'Đã xảy ra lỗi khi tìm người dùng.' });
